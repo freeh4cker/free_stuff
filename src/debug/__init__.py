64caa2be526054698ce7138715f41694a8c2d19a
free hacker <freeh4cker@gmail.com>
2016-03-23 13:58:24 +0100

    add licence ref in file of code & fix date in copyrigth

--- src/debug/__init__.py
+++ src/debug/__init__.py
@@ -1,6 +1,8 @@
-# Copyright (c) 2014 Bertrand Néron. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+##########################################################################
+# Copyright (c) 2015 Bertrand Néron. All rights reserved.                #
+# Use of this source code is governed by a BSD-style license that can be #
+# found in the LICENSE file.                                             #
+##########################################################################
 
 __author__ = 'bertrand Néron'
 
